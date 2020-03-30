e19a9c11b6142b503a8f94f2b5d3ab2fa9eb9277
Mayank Kataruka <mayank.kt9@gmail.com>
2020-03-29 21:38:07 -0700

    Files

--- Graph_Programming/src/disjoint_ds/Disjoint_DS.java
+++ Graph_Programming/src/disjoint_ds/Disjoint_DS.java
@@ -3,59 +3,54 @@ package disjoint_ds;
 import java.util.Arrays;
 
 public class Disjoint_DS {
-	int[] rank, parent; 
-    int n; 
-    
-    public Disjoint_DS(int n) 
-    { 
-        rank = new int[n]; 
-        parent = new int[n]; 
-        this.n = n; 
-        makeSet(); 
-    }
-	
-    void makeSet() 
-    { 
-        for (int i = 0; i < n; i++) {
-            parent[i] = i; 
-        } 
-    } 
-    
-    int find(int x) 
-    { 
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); 
-        } 
-        return parent[x]; 
-    } 
-    
-    void union(int x, int y) 
-    { 
-        int xParent = find(x), yParent = find(y); 
-        if (xParent == yParent) 
-            return; 
-        if (rank[xParent] < rank[yParent]) 
-            parent[xParent] = yParent; 
-        else if (rank[yParent] < rank[xParent]) 
-            parent[yParent] = xParent; 
-        else  
-        { 
-            parent[yParent] = xParent; 
-            rank[xParent] = rank[xParent] + 1; 
-        } 
-    } 
-    
-    void printParent()
-    {
-    	System.out.println(Arrays.toString(parent));
-    }
-    
+	int[] rank, parent;
+	int n;
+
+	public Disjoint_DS(int n) {
+		rank = new int[n];
+		parent = new int[n];
+		this.n = n;
+		makeSet();
+	}
+
+	void makeSet() {
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+	}
+
+	int find(int x) {
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
+
+	void union(int x, int y) {
+		int xParent = find(x);
+		int yParent = find(y);
+		if (xParent == yParent)
+			return;
+		if (rank[xParent] < rank[yParent])
+			parent[xParent] = yParent;
+		else if (rank[yParent] < rank[xParent])
+			parent[yParent] = xParent;
+		else {
+			parent[yParent] = xParent;
+			rank[xParent] = rank[xParent] + 1;
+		}
+	}
+
+	void printParent() {
+		System.out.println(Arrays.toString(parent));
+	}
+
 	public static void main(String[] args) {
-		int n=7;
+		int n = 7;
 		Disjoint_DS dj = new Disjoint_DS(n);
-		
+
 		dj.printParent();
-		
+
 		dj.union(0, 1);
 		dj.printParent();
 		dj.union(3, 4);
@@ -70,10 +65,6 @@ public class Disjoint_DS {
 		dj.printParent();
 		dj.union(2, 6);
 		dj.printParent();
-		
-		
-		
-		
-		
+
 	}
 }
